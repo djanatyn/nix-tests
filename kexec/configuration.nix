e91e1c72a6572e7b026322a6edb2f2aee1ce19ab
Jonathan Strickland <djanatyn@gmail.com>
2020-08-12 18:22:44 -0400

    ovhcloud kexec nixos install

--- kexec/configuration.nix
+++ kexec/configuration.nix
@@ -5,16 +5,46 @@
 with lib;
 
 {
-  imports = [ <nixpkgs/nixos/modules/installer/netboot/netboot-minimal.nix> ./autoreboot.nix ./kexec.nix ./justdoit.nix ];
+  imports = [
+    <nixpkgs/nixos/modules/installer/netboot/netboot-minimal.nix>
+    ./autoreboot.nix
+    ./kexec.nix
+    ./justdoit.nix
+  ];
 
   boot.supportedFilesystems = [ "zfs" ];
   boot.loader.grub.enable = false;
   boot.kernelParams = [
-    "console=ttyS0,115200"          # allows certain forms of remote access, if the hardware is setup right
-    "panic=30" "boot.panic_on_fail" # reboot the machine upon fatal boot issues
+    "console=ttyS0,115200" # allows certain forms of remote access, if the hardware is setup right
+    "panic=30"
+    "boot.panic_on_fail" # reboot the machine upon fatal boot issues
   ];
   systemd.services.sshd.wantedBy = mkForce [ "multi-user.target" ];
-  networking.hostName = "kexec";
+
+  boot.kernelModules = [ "kvm-intel" ];
+  boot.initrd.availableKernelModules = [ "ata_piix" "uhci_hcd" "virtio_pci" ];
+
   # example way to embed an ssh pubkey into the tar
-  # users.users.root.openssh.authorizedKeys.keys = [ "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC34wZQFEOGkA5b0Z6maE3aKy/ix1MiK1D0Qmg4E9skAA57yKtWYzjA23r5OCF4Nhlj1CuYd6P1sEI/fMnxf+KkqqgW3ZoZ0+pQu4Bd8Ymi3OkkQX9kiq2coD3AFI6JytC6uBi6FaZQT5fG59DbXhxO5YpZlym8ps1obyCBX0hyKntD18RgHNaNM+jkQOhQ5OoxKsBEobxQOEdjIowl2QeEHb99n45sFr53NFqk3UCz0Y7ZMf1hSFQPuuEC/wExzBBJ1Wl7E1LlNA4p9O3qJUSadGZS4e5nSLqMnbQWv2icQS/7J8IwY0M8r1MsL8mdnlXHUofPlG1r4mtovQ2myzOx clever@nixos" ];
+  users.users.root.openssh.authorizedKeys.keys = [
+    "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDFQPTKrT397qtitl0hHkl3HysPfnpEm/WmO9f4dC4kLkrHIgs2t9Yvd6z+8C/hufW+e0cVug3sb6xHWFI78+/eCSRQpPWVsE3e6/U5R/EGJqylPLEa/SmB4hB6LpsCnJkeHnD/sVBz/EjFD29wifLFq0Y5keMdxbvUMjkGrep0CD1guYseFJOdFpLF3A5GAnnP2CHgvOT7/Pd2mym5f2Mxp17SF1iYAsx9xId5o6YbmKldz3BN51N+9CROSg9QWuSNCvA7qjflBIPtnBVZFvIN3U56OECZrv9ZY4dY2jrsUGvnGiyBkkdxw4+iR9g5kjx9jPnqZJGSEjWOYSl+2cEQGvvoSF8jPiH8yLEfC+CyFrb5FMbdXitiQz3r3Xy+oLhj8ULhnDdWZpRaJYTqhdS12R9RCoUQyP7tlyMawMxsiCUPH/wcaGInzpeSLZ5BSzVFhhMJ17TX+OpvIhWlmvpPuN0opmfaNGhVdBGFTNDfWt9jjs/OHm6RpVXacfeflP62xZQBUf3Hcat2JOqj182umjjZhBPDCJscfv52sdfkiqwWIc/GwdmKt5HqU+dX7lCFJ1OGF2ymnGEnkUwW+35qX8g2P+Vc4s28MmaO5M1R5UsMFnhtFbLdfLFKn2PEvepvIqyYFMziPzEBya4zBUch/9sd6UN3DV+rA/JB/rBApw== djanatyn@nixos"
+    "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBIOiCqSWnlyk3Efun+zeqeR9afQ3gwYV0QF2l9Us15F8BnNkEqZMvVYQipZUJKwyV4P8X7yJP+2G/KGVhW5kG+4= flowercluster"
+  ];
+
+  /* example usage:
+
+     $ mkfifo kexec-gateway.socket
+     $ mkfifo kexec-ip.socket
+     $ echo 1.2.3.4 > kexec-gateway.socket &
+     $ echo 5.6.7.8 > kexec-ip.socket &
+  */
+  networking = {
+    hostName = "kexec";
+    useDHCP = false;
+    defaultGateway = lib.fileContents "kexec-gateway.socket";
+    nameservers = [ "8.8.8.8" ];
+    interfaces."enp1s0f0".ipv4.addresses = [{
+      address = lib.fileContents "kexec-ip.socket";
+      prefixLength = 32;
+    }];
+  };
 }
